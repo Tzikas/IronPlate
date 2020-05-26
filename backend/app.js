fbfb869c09d0a4a800ce80e58b83d32319c60ea2
Tzikas <niko.tzikas@gmail.com>
2020-05-26 14:26:49 -0400

    deploying backend to heroku

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const passport = require('./config/passport');
 
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ironplate'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/deploymentExample'
 console.log('Connecting DB to ', MONGODB_URI)
 
 mongoose
