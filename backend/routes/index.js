6bc3bd133a5c58932750e76eb92f8ec9eba15f40
abbanat <gja1294@aol.com>
2020-05-27 18:01:39 -0400

    code update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 router.get('/', (req, res, next) => {
   console.log('bla')
   res.status(200).json({ msg: 'Working' });
 });
 
-router.post('/cocktail', (req, res, next) => {
+router.post('/cocktail', isAuth, (req, res, next) => {
   console.log('hi from back')
-  console.log(req)
+  console.log(req.body, req.user)
+  User.findByIdAndUpdate(req.user._id, {$push: {favCocktails: req.body}}) .then(user => {
+    res.json(user)
+  })
 })
+function isAuth(req, res, next) {
+  req.isAuthenticated() ? next() : res.status(401).json({ msg: 'Log in first' });
+}
+
+
+
 
 module.exports = router;
