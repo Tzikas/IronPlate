159c17b75022d3f0c377ed654c9c464cf3c983af
Tzikas <niko.tzikas@gmail.com>
2020-08-13 17:46:32 -0400

    fixed andrew issue

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -3,83 +3,99 @@ import { Redirect } from 'react-router-dom'
 import TheContext from '../../TheContext'
 import actions from '../../api'
 import moment from 'moment'
-import {NotificationManager} from 'react-notifications';
+import { NotificationManager } from 'react-notifications';
 
 const Profile = (props) => {
     const [posts, setPosts] = useState([])
     const [otherPosts, setOtherPosts] = useState([])
-      useEffect(() => { 
+    useEffect(() => {
 
         //Get my posts 
         actions.getMyPosts().then(posts => {
-           if(posts)
-            setPosts(posts.data.reverse())
-          }).catch(err => console.error(err))     
+            if (posts)
+                setPosts(posts.data.reverse())
+        }).catch(err => console.error(err))
 
         //Get other posts
         actions.getOtherPosts().then(posts => {
-           if(posts)
-            setOtherPosts(posts.data.reverse())
-          }).catch(err => console.error(err))    
+            if (posts)
+                setOtherPosts(posts.data.reverse())
+        }).catch(err => console.error(err))
 
-      }, [])
+    }, [])
 
-  
+
     return (
         <div>
-            
+
             <Welcome />  {/*'Look ma!  No props!!!'*/}
             <AddPost {...props} posts={posts} />
-            <MyPosts posts={posts} setPosts={setPosts}/>
+            <MyPosts posts={posts} setPosts={setPosts} />
             <OthersPosts posts={otherPosts} />
-             
+
         </div>
     );
 }
 
-function MyPosts({posts, setPosts}){
+function MyPosts({ posts, setPosts }) {
 
-    let rows = posts.map((post, i)=> <EachMyPost key={post._id} post={post} posts={posts} setPosts={setPosts} i={i}/>)
+    let rows = posts.map((post, i) => <EachMyPost key={post._id} post={post} posts={posts} setPosts={setPosts} i={i} />)
 
     rows.unshift(
-        React.createElement('h2', {key:'help you'}, 'I need help with:')
+        React.createElement('h2', { key: 'help you' }, 'I need help with:')
     )
     return rows
 }
 
 
-function EachMyPost({post, posts, setPosts, i}){
+function EachMyPost({ post, posts, setPosts, i }) {
 
-    const [resolve, setResolve] = useState(post.resolved)
-    let {user, setUser} = useContext(TheContext)
-    
+    let [resolve, setResolve] = useState(post.resolved)
+    let [timeUp, setTimeup] = useState(false)
+    let [time, setTime] = useState(0)
+    let { user, setUser } = useContext(TheContext)
+
     const resolvePost = (val) => (event) => {
 
-        actions.resolvePost({post, resolved:val}).then(res => {
-            setResolve(val)
-            let newPosts = [...posts]
-            newPosts[i] = res.data.posted
-            setPosts(newPosts)
-            setUser(res.data.helpee)
-            if(val)  
-                NotificationManager.success(`You give ${post.bounty} points`, 'Issue Resolved')
-            else 
-                NotificationManager.warning(`You are refunded ${post.bounty} points`, 'Issue Still Open')
-        }).catch(err => console.error(err))
+        setResolve(val)
+        console.log(val)
+        if(val) {
+            console.log('resolve')
+            let t = setTimeout(() => {
+                console.log('timouet fired')
+                actions.resolvePost({ post, resolved: val }).then(res => {
+                    let newPosts = [...posts]
+                    newPosts[i] = res.data.posted
+                    setPosts(newPosts)
+                    setUser(res.data.helpee)
+                    if (val)
+                        NotificationManager.success(`You give ${post.bounty} points`, 'Issue Resolved')
+                    else
+                        NotificationManager.warning(`You are refunded ${post.bounty} points`, 'Issue Still Open')
+                }).catch(err => console.error(err))
+
+                setTimeup(true)
+            }, 5000)
+            setTime(t)
+        } else {
+            clearTimeout(time)
+        }
 
     }
-  
-    const cancelPost = (event) => {
-        console.log(i, posts)
-        actions.cancelPost({post}).then(res => {
-                console.log(res, res.data.user, 'hmmm')
-                let newPosts = [...posts]
-                newPosts.splice(i,1)
-                setPosts(newPosts)
-                setUser(res.data.user)
-                NotificationManager.success( `You are refunded ${post.bounty} points`,'Issue Deleted',)
 
-        }).catch(err=> console.error(err))
+
+
+    const cancelPost = (event) => {
+        console.log(i, posts, time)
+        actions.cancelPost({ post }).then(res => {
+            console.log(res, res.data.user, 'hmmm')
+            let newPosts = [...posts]
+            newPosts.splice(i, 1)
+            setPosts(newPosts)
+            setUser(res.data.user)
+            NotificationManager.success(`You are refunded ${post.bounty} points`, 'Issue Deleted')
+
+        }).catch(err => console.error(err))
 
     }
 
@@ -87,47 +103,47 @@ function EachMyPost({post, posts, setPosts, i}){
     return (
         <li key={post._id}>
             <div>{post.message}  <i>{post.bounty} Points</i> <i><img src={post.helper?.imageUrl} /> {post.helper?.name}</i> </div>
-                {post.helper?
-                    resolve ? 
-                        <button onClick={resolvePost(false)}>Undo<h2>🔴</h2></button>
-                        : 
-                        <button onClick={resolvePost(true)}>Resolve<h2>✅</h2></button>
-                 : <button onClick={cancelPost}>Cancel</button>}
+            {post.helper ?
+                resolve ?
+                    <button disabled={timeUp} onClick={resolvePost(false)}>Undo<h2>🔴</h2></button>
+                    :
+                    <button onClick={resolvePost(true)}>Resolve<h2>✅</h2></button>
+                : <button onClick={cancelPost}>Cancel</button>}
         </li>
     )
 }
 
 
-function OthersPosts({posts}){
+function OthersPosts({ posts }) {
     let rows = posts.map(eachPost => (
-    
-          <li key={eachPost._id}>
-            <div>{eachPost.message}  
-                <i>{eachPost.bounty}</i> 
-                <i><img src={eachPost.user?.imageUrl} /></i> 
+
+        <li key={eachPost._id}>
+            <div>{eachPost.message}
+                <i>{eachPost.bounty}</i>
+                <i><img src={eachPost.user?.imageUrl} /></i>
                 <i>Created {moment(eachPost.createdAt).format('h:mm:ss a')}</i>
                 <i>Last updated {moment(eachPost.updatedAt).format('h:mm:ss a')}</i>
             </div>
-           
-            <div>You're helping {eachPost.user?.name}</div>
-    
-          
 
-          </li>
-    
+            <div>You're helping {eachPost.user?.name}</div>
+
+
+
+        </li>
+
     ))
     rows.unshift(
-        React.createElement('h2', {key:'help me'}, 'Im helping:')
+        React.createElement('h2', { key: 'help me' }, 'Im helping:')
     )
     return rows
 }
 
 
 
-const AddPost = ({history, posts}) => {
+const AddPost = ({ history, posts }) => {
     const [message, setMessage] = useState("")
-    
-    let {user, setUser} = React.useContext(TheContext); 
+
+    let { user, setUser } = React.useContext(TheContext);
 
     //See if there is enough points to ask a question
     //let totalPointsSpent = posts.reduce((acc,post) => {
@@ -146,23 +162,23 @@ const AddPost = ({history, posts}) => {
     const handleSubmit = e => {
         e.preventDefault();
 
-        actions.addPost({message}).then(res => { 
+        actions.addPost({ message }).then(res => {
             console.log(res)
             setUser(res.data.user)
             NotificationManager.info(`You've submitted a new issue`)
             history.push('/')
-        }).catch(err=> console.error(err))
+        }).catch(err => console.error(err))
 
     }
 
 
-    return ( 
+    return (
         <Fragment>
-                    
-            <form  id="askForHelp" onSubmit={handleSubmit}>
-                <input  disabled={outOfPoints} onChange={(e) => setMessage(e.target.value)} placeholder={outOfPoints? "Sorry your out of points" : "What's your issue?"} type="text" />
+
+            <form id="askForHelp" onSubmit={handleSubmit}>
+                <input disabled={outOfPoints} onChange={(e) => setMessage(e.target.value)} placeholder={outOfPoints ? "Sorry your out of points" : "What's your issue?"} type="text" />
                 <button disabled={outOfPoints}>Add</button>
-            
+
             </form>
         </Fragment>
 
@@ -172,11 +188,11 @@ const AddPost = ({history, posts}) => {
 
 
 const Welcome = () => {
-    
-    let {user, history} = React.useContext(TheContext); //With Context I can skip the prop drilling and access the context directly 
+
+    let { user, history } = React.useContext(TheContext); //With Context I can skip the prop drilling and access the context directly 
 
     if (!user) {
-        return <Redirect to='/'/>;
+        return <Redirect to='/' />;
     }
     return (
         <div className="profile">
@@ -192,28 +208,28 @@ const Welcome = () => {
 function notifyMe(message) {
     // Let's check if the browser supports notifications
     if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
+        alert("This browser does not support desktop notification");
     }
-  
+
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
-      // If it's okay let's create a notification
-      var notification = new Notification(message);
+        // If it's okay let's create a notification
+        var notification = new Notification(message);
     }
-  
+
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          var notification = new Notification(message);
-        }
-      });
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(message);
+            }
+        });
     }
-  
+
     // At last, if the user has denied notifications, and you 
     // want to be respectful there is no need to bother them any more.
-  }
+}
 
 
 export default Profile;
